24df928dc2d36c67aaf59658b1fb4b209c16e5a2
ampersand-paris <ampersand.s.paris@gmail.com>
2022-06-03 18:27:07 -0400

    landing page working

---
+++ src/Footer.js
@@ -0,0 +1,13 @@
+/* === React Elements === */
+import React from "react";
+
+const Footer = () => {
+
+    return (
+        <div className="bg-vermillion no-margin side-padding">
+            <h2 className="white">This is the footer.</h2>
+        </div>
+    )
+}
+
+export default Footer;
