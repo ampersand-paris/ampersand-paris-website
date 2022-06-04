22fbec2a6c8b1e0a49c195da48b011df9917103e
ampersand-paris <ampersand.s.paris@gmail.com>
2022-06-04 11:43:55 -0400

    menu reconfigure

---
+++ src/Ampersand/Ampersand.js
@@ -0,0 +1,13 @@
+/* === React Elements === */
+import React from "react";
+
+const Ampersand = () => {
+
+    return (
+        <div className="bg-vermillion no-margin side-padding">
+            <h2 className="white">Ampersand</h2>
+        </div>
+    )
+}
+
+export default Ampersand;
--- src/App.js
+++ src/App.js
@@ -6,11 +6,11 @@ function App() {
       <div className="flex">
         <div className="half full-height flex bg-bright-red">
           <div className="z-index half h-one-hundred flex column just-center side-padding left-menu bg-bright-red">
-            <h2 className="white">fermentation CSA</h2>
-            <h2 className="white">fermentorship</h2>
-            <h2 className="white">recipes</h2>
+            <h2 className="white">portfolio</h2>
+            <h2 className="white">resume</h2>
+            <h2 className="white"></h2>
             <h2 className="white">writings</h2>
-            <h2 className="white">FAILSPACE</h2>
+            <h2 className="white">FAILSPACE Design Services</h2>
           </div>
         </div>
         <div className="full-height full-width flex just-center absolute">
@@ -19,11 +19,12 @@ function App() {
         </div>
         <div className="half full-height flex al-center flex-end txt-al-right bg-white">
           <div className="z-index half h-one-hundred flex column just-center side-padding left-menu bg-white">
-            <h2 className="bright-red">portfolio</h2>
-            <h2 className="bright-red">resume</h2>
-            <h2 className="bright-red"></h2>
+            <h2 className="bright-red">ampersand</h2>
+            <h2 className="bright-red">fermentation CSA</h2>
+            <h2 className="bright-red">fermentorship</h2>
+            <h2 className="bright-red">recipes</h2>
             <h2 className="bright-red">writings</h2>
-            <h2 className="bright-red">FAILSPACE Design Services</h2>
+            <h2 className="bright-red">FAILSPACE</h2>
           </div>
         </div>
       </div>
