817e5d2cc9cddf19a2534227fbcaee248a34c898
ampersand-paris <ampersand.s.paris@gmail.com>
2022-06-04 12:39:10 -0400

    menu toggle

--- src/Ampersand/Ampersand.js
+++ src/Ampersand/Ampersand.js
@@ -1,10 +1,13 @@
 /* === React Elements === */
 import React from "react";
 
-const Ampersand = () => {
+const Ampersand = (props) => {
 
+    const contentStyles = {
+        display: props.active,
+    }
     return (
-        <div className="bg-vermillion no-margin side-padding">
+        <div style={contentStyles} className="bg-vermillion no-margin side-padding">
             <h2 className="white">Ampersand</h2>
         </div>
     )
--- src/App.js
+++ src/App.js
@@ -1,11 +1,34 @@
-import Footer from './Footer'
+import { BrowserRouter as Router,
+  Routes,
+  Route,
+  Link } from "react-router-dom";
+import { useState } from "react";
+
+/* === Components === */
+import Footer from './Footer';
+import Ampersand from './Ampersand/Ampersand'
 
 function App() {
+  const [whiteBackground, setWhiteBackground] = useState('50vW');  
+  const [redBackground, setRedBackground] = useState('50vW');  
+
+  const [ampersand, setAmpersand] = useState('none')
+
+  const toggleWhiteBackground = () => {
+    setWhiteBackground('100vW')
+    setRedBackground('0vW')
+  }
+
+  const toggleAmpersand = () => {
+    setAmpersand('flex')
+  }
+
+
   return (
-    <>
+    <Router>
       <div className="flex">
-        <div className="half full-height flex bg-bright-red">
-          <div className="z-index half h-one-hundred flex column just-center side-padding left-menu bg-bright-red">
+        <div style={{width: redBackground}} className="full-height flex bg-bright-red">
+          <div style={{width: redBackground}} className="z-index h-one-hundred flex column just-center side-padding left-menu bg-bright-red">
             <h2 className="white">portfolio</h2>
             <h2 className="white">resume</h2>
             <h2 className="white"></h2>
@@ -17,9 +40,10 @@ function App() {
           <img className="ampersand" src="/Images/left-half-ampersand.svg" />
           <img className="ampersand" src="/Images/right-half-ampersand.svg" />
         </div>
-        <div className="half full-height flex al-center flex-end txt-al-right bg-white">
-          <div className="z-index half h-one-hundred flex column just-center side-padding left-menu bg-white">
-            <h2 className="bright-red">ampersand</h2>
+        <div style={{width: whiteBackground}} className="full-height flex al-center flex-end txt-al-right bg-white">
+          <Ampersand active={ampersand}/>
+          <div style={{width: whiteBackground}} className="z-index  h-one-hundred flex column just-center side-padding left-menu bg-white">
+            <Link to="/ampersand"><h2 onClick={() => {toggleWhiteBackground(); toggleAmpersand()}} className="bright-red">ampersand</h2></Link>
             <h2 className="bright-red">fermentation CSA</h2>
             <h2 className="bright-red">fermentorship</h2>
             <h2 className="bright-red">recipes</h2>
@@ -29,7 +53,10 @@ function App() {
         </div>
       </div>
       <Footer />
-    </>
+      <Routes>
+        <Route path="/ampersand" element={<Ampersand active={ampersand} />} />
+      </Routes>
+    </Router>
   );
 }
 
