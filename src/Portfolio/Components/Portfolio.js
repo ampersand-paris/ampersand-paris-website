71b0be15002ba5d0418547940567602fbad440df
ampersand-paris <ampersand.s.paris@gmail.com>
2022-06-08 20:01:45 -0400

    portfolio update

--- src/Portfolio/Components/Portfolio.js
+++ src/Portfolio/Components/Portfolio.js
@@ -2,6 +2,8 @@
 import React from "react";
 
 import failspaceBackground from "./../PortfolioImages/FAILSPACE.png"
+import YCCLABackground from "./../PortfolioImages/YCCLA.png"
+import PRIORIBackground from "./../PortfolioImages/PRIORI.png"
 
 const Portfolio = () => {
 
@@ -22,13 +24,12 @@ const Portfolio = () => {
                     <p className="white tech-padding">Netlify</p>
                 </div>
                 <div className="flex no-space">
-                    <img className="icon icon-padding-first" src="./Images/GitHub.png" />
-                    <img className="icon icon-padding" src="./Images/ExternalLink.png" />
+                    <a href="https://github.com/ampersand-paris/FAILSPACE"><img className="icon icon-padding-first" src="./Images/GitHub.png" /></a>
+                    <a href="https://failspacenyc.com"><img className="icon icon-padding" src="./Images/ExternalLink.png" /></a>
                 </div>
             </div>
             <div style={{ backgroundImage: `url(${failspaceBackground})` }} className="portfolio-image absolute right" id="failspace">
                 <div className="portfolio-image-overlay">
-
                 </div>
             </div>
         </div>
@@ -36,13 +37,57 @@ const Portfolio = () => {
             <div className="portfolio-text relative z-index">
                 <h3 className="white portfolio-title txt-al-right"><span className="opacity-80">Featured Project |<br></br></span> You Can Cook Literally Anything</h3>
                 <div className="bg-white border-radius text-padding">
+                    <p className="txt-al-right">Web application developed for recipe collection and recipe testing. Users can keep track of their correspondance, search published recipes, and, for selected users, create and share their thoughts on recipes in development.</p>
+                </div>
+                <div className="flex flex-end no-space">
+                    <p className="white tech-padding">Django</p>
+                    <p className="white tech-padding">PostgreSQL</p>
+                    <p className="white tech-padding">CSS</p>
+                    <p className="white tech-padding-first-reverse">Heroku</p>
+                </div>
+                <div className="flex flex-end no-space">
+                    <a href="https://github.com/ampersand-paris/YCCLAv2"><img className="icon icon-padding-first" src="./Images/GitHub.png" /></a>
+                    <a href="https://ycclav2.herokuapp.com/"><img className="icon icon-padding" src="./Images/ExternalLink.png" /></a>
+                </div>
+            </div>
+            <div style={{ backgroundImage: `url(${YCCLABackground})` }} className="portfolio-image absolute left" id="failspace">
+                <div className="portfolio-image-overlay">
+                </div>
+            </div>
+        </div>
+        <div className="seventy-five relative top-padding">
+            <div className="portfolio-text relative z-index">
+                <h3 className="white portfolio-title"><span className="opacity-80">Featured Project |</span> PRIORI</h3>
+                <div className="bg-white  border-radius text-padding">
+                    <p>Prioritization web application that allows users to make to-do lists and prioritize a maxiumum of three tasks per day.</p>
+                </div>
+                <div className="flex no-space">
+                    <p className="white tech-padding-first">Django</p>
+                    <p className="white tech-padding">PostgreSQL</p>
+                    <p className="white tech-padding">CSS</p>
+                    <p className="white tech-padding">Heroku</p>
+                </div>
+                <div className="flex no-space">
+                    <a href="https://github.com/ampersand-paris/PRIORI"><img className="icon icon-padding-first" src="./Images/GitHub.png" /></a>
+                    <a href="https://ycclav2.herokuapp.com/"><img className="icon icon-padding" src="./Images/ExternalLink.png" /></a>
+                </div>
+            </div>
+            <div style={{ backgroundImage: `url(${PRIORIBackground})` }} className="portfolio-image absolute right" id="failspace">
+                <div className="portfolio-image-overlay">
+
+                </div>
+            </div>
+        </div>
+        <div className="seventy-five relative top-padding flex flex-end">
+            <div className="portfolio-text relative z-index">
+                <h3 className="white portfolio-title txt-al-right"><span className="opacity-80">Featured Project |</span> Reaxion</h3>
+                <div className="bg-white border-radius text-padding">
                     <p className="txt-al-right">A boldly designed web application that serves as the digital home for a design studio and performing arts mentorship organization in New York City.</p>
                 </div>
                 <div className="flex flex-end no-space">
-                    <p className="white tech-padding-first">Django</p>
-                    <p className="white tech-padding-first">PostgreSQL</p>
-                    <p className="white tech-padding">CSS</p>
-                    <p className="white tech-padding">Heroku</p>
+                    <p className="white tech-padding">React</p>
+                    <p className="white tech-padding">MongoDB</p>
+                    <p className="white tech-padding-first-reverse">CSS</p>
                 </div>
                 <div className="flex flex-end no-space">
                     <img className="icon icon-padding-first" src="./Images/GitHub.png" />
@@ -56,14 +101,16 @@ const Portfolio = () => {
         </div>
         <div className="seventy-five relative top-padding">
             <div className="portfolio-text relative z-index">
-                <h3 className="white portfolio-title"><span className="opacity-80">Featured Project |</span> PRIORI</h3>
+                <h3 className="white portfolio-title"><span className="opacity-80">Featured Project |</span> BUNBUNS</h3>
                 <div className="bg-white  border-radius text-padding">
                     <p>A boldly designed web application that serves as the digital home for a design studio and performing arts mentorship organization in New York City.</p>
                 </div>
                 <div className="flex no-space">
-                    <p className="white tech-padding-first">React</p>
+                    <p className="white tech-padding-first">EJS</p>
+                    <p className="white tech-padding">Node.js</p>
+                    <p className="white tech-padding">Express</p>
+                    <p className="white tech-padding">MongoDB</p>
                     <p className="white tech-padding">CSS</p>
-                    <p className="white tech-padding">Netlify</p>
                 </div>
                 <div className="flex no-space">
                     <img className="icon icon-padding-first" src="./Images/GitHub.png" />
@@ -73,27 +120,6 @@ const Portfolio = () => {
             <div style={{ backgroundImage: `url(${failspaceBackground})` }} className="portfolio-image absolute right" id="failspace">
                 <div className="portfolio-image-overlay">
 
-                </div>
-            </div>
-        </div>
-        <div className="seventy-five relative top-padding flex flex-end">
-            <div className="portfolio-text relative z-index">
-                <h3 className="white portfolio-title txt-al-right"><span className="opacity-80">Featured Project |</span> Reaxion</h3>
-                <div className="bg-white border-radius text-padding">
-                    <p className="txt-al-right">A boldly designed web application that serves as the digital home for a design studio and performing arts mentorship organization in New York City.</p>
-                </div>
-                <div className="flex flex-end no-space">
-                    <p className="white tech-padding-first">React</p>
-                    <p className="white tech-padding-first">MongoDB</p>
-                    <p className="white tech-padding">CSS</p>
-                </div>
-                <div className="flex flex-end no-space">
-                    <img className="icon icon-padding-first" src="./Images/GitHub.png" />
-                    <img className="icon icon-padding" src="./Images/ExternalLink.png" />
-                </div>
-            </div>
-            <div style={{ backgroundImage: `url(${failspaceBackground})` }} className="portfolio-image absolute left" id="failspace">
-                <div className="portfolio-image-overlay">
                 </div>
             </div>
         </div>
