bce3094c0934496cc57f96383d68cd9c60a4f1d2
ampersand-paris <ampersand.s.paris@gmail.com>
2022-06-14 16:41:27 -0400

    right menu functions

--- src/Menus/RightMenu.js
+++ src/Menus/RightMenu.js
@@ -10,14 +10,59 @@ const RightMenu = (props) => {
         display: (props.display),
         opacity: (props.opacity)
     }
+
+    const scrollAmpersand = () => {
+        if (window.location.href === "http://localhost:3000/") {
+            window.location.href = "http://localhost:3000/ampersand"
+        } else {
+            const element = document.getElementById("ampersand");
+            element.scrollIntoView({ behavior: 'smooth' });
+        }
+    }
+    
+    const scrollCSA = () => {
+        if (window.location.href === "http://localhost:3000/") {
+            window.location.href = "http://localhost:3000/about#CSA"
+        } else {
+            const element = document.getElementById("CSA");
+            element.scrollIntoView({ behavior: 'smooth' });
+        }
+    }
+
+    const scrollProjects = () => {
+        if (window.location.href === "http://localhost:3000/") {
+            window.location.href = "http://localhost:3000/about#projects"
+        } else {
+            const element = document.getElementById("projects");
+            element.scrollIntoView({ behavior: 'smooth' });
+        }
+    }
+
+    const scrollRecipes = () => {
+        if (window.location.href === "http://localhost:3000/") {
+            window.location.href = "http://localhost:3000/about#recipes"
+        } else {
+            const element = document.getElementById("recipes");
+            element.scrollIntoView({ behavior: 'smooth' });
+        }
+    }
+
+    const scrollWritings = () => {
+        if (window.location.href === "http://localhost:3000/") {
+            window.location.href = "http://localhost:3000/about#writings"
+        } else {
+            const element = document.getElementById("writings");
+            element.scrollIntoView({ behavior: 'smooth' });
+        }
+    }
     
     return (
         <div style={contentStyles} className="z-index  h-one-hundred flex column just-center side-padding right-menu txt-al-right">
-            <Link to="/ampersand"><h3 onClick={props.active} className="bright-red menu">ampersand</h3></Link>   
-            <h3 className="bright-red menu">fermentation CSA</h3>
-            <h3 className="bright-red menu">fermentorship</h3>
-            <h3 className="bright-red menu">recipes</h3>
-            <h3 className="bright-red menu">writings</h3>
+            <h3 onClick={scrollAmpersand} className="bright-red menu">ampersand</h3>
+            <h3 onClick={scrollCSA} className="bright-red menu">fermentation CSA</h3>
+            <h3 onClick={scrollProjects} className="bright-red menu">projects</h3>
+            <h3 onClick={scrollRecipes} className="bright-red menu">recipes</h3>
+            <h3 onClick={scrollWritings} className="bright-red menu">writings</h3>
         </div>
     )
 }
