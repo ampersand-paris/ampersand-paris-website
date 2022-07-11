ae6e0a578296819a3a30837aef7a6eba3e3362da
ampersand-paris <ampersand.s.paris@gmail.com>
2022-07-11 15:57:06 -0400

    arrow logic

--- src/Ampersand/Components/Recipes.js
+++ src/Ampersand/Components/Recipes.js
@@ -14,66 +14,96 @@ const Recipes = () => {
     const [oatmealTwoWays, setOatmealTwoWays] = useState('none')
     const [midProcess, setMidProcess] = useState('none')
 
-    const [arrowTop, setArrowTop] = useState('flex')
-    const [arrowBottom, setArrowBottom] = useState('none')
+    const [arrowOne, setArrowOne] = useState('flex')
+    const [arrowTwo, setArrowTwo] = useState('none')
+    const [arrowThree, setArrowThree] = useState('none')
+    const [arrowFour, setArrowFour] = useState('none')
+
 
     const toggleFennelFullDish = () => {
+        // Recipe
         setFennelFullDish('flex')
         setPickledFennelStalks('none')
         setOatmealTwoWays('none')
         setMidProcess('none')
-
-        setArrowTop('flex')
+        // Arrow
+        setArrowOne('flex')
+        setArrowTwo('none')
+        setArrowThree('none')
+        setArrowFour('none')
     }
 
     const togglePickledFennelStalks = () => {
+        // Recipe
         setFennelFullDish('none')
         setPickledFennelStalks('flex')
         setOatmealTwoWays('none')
         setMidProcess('none')
-
-        setArrowTop('none')
-        setArrowBottom('flex')
+        // Arrow
+        setArrowOne('none')
+        setArrowTwo('flex')
+        setArrowThree('none')
+        setArrowFour('none')
     }
 
     const toggleOatmealTwoWays = () => {
+        // Recipe
         setFennelFullDish('none')
         setPickledFennelStalks('none')
         setOatmealTwoWays('flex')
         setMidProcess('none')
+        // Arrow
+        setArrowOne('none')
+        setArrowTwo('none')
+        setArrowThree('flex')
+        setArrowFour('none')
     }
 
     const toggleMidProcess = () => {
+        // Recipe
         setFennelFullDish('none')
         setPickledFennelStalks('none')
         setOatmealTwoWays('none')
         setMidProcess('flex')
+        // Arrow
+        setArrowOne('none')
+        setArrowTwo('none')
+        setArrowThree('none')
+        setArrowFour('flex')
     }
 
-    const topArrow = () => {
-        if(arrowTop === 'flex') {
-            setArrowTop('flex')
-            setArrowBottom('none')
-        } else if (arrowTop === 'none') {
-            setArrowTop('flex')
-        }
-    }        
+    // const arrowOne = () => {
+    //     if(arrowTop === 'flex') {
+    //         setArrowTop('flex')
+    //         setArrowBottom('none')
+    //     } else if (arrowTop === 'none') {
+    //         setArrowTop('flex')
+    //     }
+    // }        
 
-    const bottomArrow = () => {
-        if(arrowBottom === 'flex' && arrowTop === 'none') {
-            setArrowBottom('flex')
-            setArrowTop('none')
-        } else if (arrowBottom === 'none' && arrowTop === 'flex') {
-            setArrowBottom('flex')
-        }
-    } 
+    // const arrowTwo = () => {
+    //     if(arrowBottom === 'flex' && arrowTop === 'none') {
+    //         setArrowBottom('flex')
+    //         setArrowTop('none')
+    //     } else if (arrowBottom === 'none' && arrowTop === 'flex') {
+    //         setArrowBottom('flex')
+    //     }
+    // } 
 
-    const arrowTopHover = {
-        display: arrowTop
+    const arrowOneDisplay = {
+        display: arrowOne
     }
 
-    const arrowBottomHover = {
-        display: arrowBottom
+    const arrowTwoDisplay = {
+        display: arrowTwo
+    }
+
+    const arrowThreeDisplay = {
+        display: arrowThree
+    }
+
+    const arrowFourDisplay = {
+        display: arrowFour
     }
 
     return (
@@ -88,7 +118,7 @@ const Recipes = () => {
                                 <div  onClick={toggleFennelFullDish} className="border-right tab-padding twenty tab">
                                     <p className="no-margin">Pickled Fennel Stalks and Kumquats Over White Beans, Fennel, Blood Orange, and Kalamata Olives</p>  
                                 </div> 
-                                <div style={arrowTopHover} className="flex al-center">
+                                <div style={arrowOneDisplay} className="flex al-center">
                                     <img className="triangle" src="./Images/Triangle.png" />
                                 </div>
                             </div>
@@ -97,7 +127,7 @@ const Recipes = () => {
                                     className="border-right tab-padding twenty tab">
                                     <p className="no-margin">Pickled Fennel Stalks and Kumquats</p>   
                                 </div>
-                                <div style={arrowBottomHover} className="flex al-center">
+                                <div style={arrowTwoDisplay} className="flex al-center">
                                     <img className="triangle" src="./Images/Triangle.png" />
                                 </div>
                             </div>  
@@ -106,7 +136,7 @@ const Recipes = () => {
                                     className="border-right tab-padding twenty tab">
                                     <p className="no-margin">Oatmeal Two Ways</p>   
                                 </div>
-                                <div style={arrowBottomHover} className="flex al-center">
+                                <div style={arrowThreeDisplay} className="flex al-center">
                                     <img className="triangle" src="./Images/Triangle.png" />
                                 </div>
                             </div>  
@@ -115,7 +145,7 @@ const Recipes = () => {
                                     className="border-right tab-padding twenty tab">
                                     <p className="no-margin">Mid-Process Cocktail</p>   
                                 </div>
-                                <div style={arrowBottomHover} className="flex al-center">
+                                <div style={arrowFourDisplay} className="flex al-center">
                                     <img className="triangle" src="./Images/Triangle.png" />
                                 </div>
                             </div>  
