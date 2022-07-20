e82e0f9968bb38a1bc1957a54a49285cdc322498
ampersand-paris <ampersand.s.paris@gmail.com>
2022-07-20 09:11:20 -0400

    final touches

--- src/Footer.js
+++ src/Footer.js
@@ -9,7 +9,7 @@ const Footer = () => {
                 <a href="mailto:ampersand.s.paris@gmail.com"><img className="icon-footer icon-padding" src="./Images/MailWhite.svg" /></a>
                 <a href="https://instagram.com/ampersandparis"><img className="icon-footer icon-padding" src="./Images/Instagram_Glyph_White.png" /></a>
                 <a href="https://github.com/ampersand-paris"><img className="icon-footer icon-padding" src="./Images/GitHub.png" /></a>
-                <a href="https://linkedin.com/ampersand-paris"><img className="icon-footer icon-padding" src="./Images/LinkedInWhite.svg" /></a>
+                <a href="https://www.linkedin.com/in/ampersand-paris/"><img className="icon-footer icon-padding" src="./Images/LinkedInWhite.svg" /></a>
             </div>
             <p className="white text-al-center">Designed and Developed by 
             <br></br>Ampersand Paris</p>
