40f3f4c02c6007bc11104df0d06c8a25c46ebaa5
ampersand-paris <ampersand.s.paris@gmail.com>
2022-09-22 17:06:36 -0400

    mobile mailling list refactored

--- src/Menus/RightMenu.js
+++ src/Menus/RightMenu.js
@@ -55,9 +55,9 @@ const RightMenu = (props) => {
     
     return (
         <div className="z-index flex right-menu h-one-hundred" id="right-menu">
-            <img onClick={props.active} className="right-menu-triangle cursor" id="right-open" src="./Images/Triangle.png" />
+            <img onClick={props.active} className="right-menu-triangle cursor" id="right-open" src="./Images/RedTriangleWhiteOutline.png" />
             <div className="z-index relative menu-width h-one-hundred flex column just-center menu-side-padding txt-al-right vermillion-border-left bg-white">
-                <img onClick={props.active} className="absolute right-menu-triangle-close cursor cursor" id="right-close" src="./Images/Triangle.png" />
+                <img onClick={props.active} className="absolute right-menu-triangle-close cursor cursor" id="right-close" src="./Images/RedTriangleWhiteOutline.png" />
                 <h3 className="absolute sub-title-right bright-red opacity-80 text-al-right">Creative</h3>       
                 <h3 onClick={scrollAmpersand} className="bright-red menu cursor">ampersand</h3>
                 <h3 onClick={scrollCSA} className="bright-red menu cursor">fermentation CSA</h3>
